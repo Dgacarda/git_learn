e8acd57ff4e853babdf4eb4aaf8c09eb47c39ccd
Alex <Dgacarda@gmail.com>
2019-05-14 08:37:46 +0500

    review code (delete jQuery, create file for scripts)

--- main.js
+++ main.js
@@ -1,44 +1,56 @@
-window.onload = function() {
+window.onload = function () {
 	// Массив гласных букв
 	const letters = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"]
 	// Текст ошибки
 	const errorMessage = 'Поставьте восклицательный знак в конец строки!'
-	 
-	// Слушаем событие клика
-	$("#searchButton").on("click", findValue())
+	// Кнопка "Поиск"
+	const btn = document.getElementById('searchButton')
 
-	// Текст, введенный пользователем
-	function findValue() {
-		const val = $("#input").val().split("")
-		searching(val)
+	btn.onclick = function() {
+		// Текстовый блок для записи количества гласных
+		const output = document.getElementById('output')
+		// Буквы, введенные пользователем
+		const val = document.getElementById('input').value
+		// Количество гласных
+		const count = searching(val)
+		
+		success(count, output)
 	}
-
+	
+	// Заполнение поля количеством найденных гласных
+	function success(count, output) {
+		output.innerHTML = count
+	}
+	
 	// Перебор введенного текста пользователем
 	function searching(string) { 
 		let count = 0
+		array = string.split('')
 		
-		if (string[string.length - 1] === '!') {
+		
+		if (array[array.length - 1] === '!') { // Проверка на обязательный символ '!' в конце строки
 			
-			string.forEach(item => {
+			array.forEach(item => { // Перебор всех букв в введенной строке
 				item.toLowerCase()
+				
 				if (letters.includes(item)) { 
 					count++ 
 				} 
-			}) 
+			})
 		  
-			success(count)
+			return count
 		} else {
 			showAlert()
+			return ''
 		}
 	}
-
+	
 	// Отображение ошибки
 	function showAlert() {
 		alert(errorMessage)
 	}
-
-	// Заполнение поля количеством найденных гласных
-	function success(count) {
-		$("#output").html(count)
-	}
 }
+
+
+	
+	

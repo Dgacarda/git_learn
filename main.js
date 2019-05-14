78956efad046f68ebc9ae9d35ec450822860c63d
Alex <Dgacarda@gmail.com>
2019-05-14 08:59:18 +0500

    add style.css for error block to show error message

--- main.js
+++ main.js
@@ -2,24 +2,39 @@ window.onload = function () {
 	// Массив гласных букв
 	const letters = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"]
 	// Текст ошибки
-	const errorMessage = 'Поставьте восклицательный знак в конец строки!'
+	const errorMessage = 'Необходимо поставить восклицательный знак в конце строки!'
 	// Кнопка "Поиск"
 	const btn = document.getElementById('searchButton')
 
 	btn.onclick = function() {
 		// Текстовый блок для записи количества гласных
 		const output = document.getElementById('output')
+		// Текстовый блок для записи ошибки
+		const errorDiv = document.getElementById('errorBlock')
 		// Буквы, введенные пользователем
 		const val = document.getElementById('input').value
 		// Количество гласных
 		const count = searching(val)
 		
-		success(count, output)
+		if (count) {
+			errorDiv.innerHTML = ''
+			success(count, output)
+		} else {
+			failure(errorDiv)
+		}
+		
+		
 	}
 	
 	// Заполнение поля количеством найденных гласных
 	function success(count, output) {
 		output.innerHTML = count
+	}
+	
+	// Исключение (некорректный ввод)
+	function failure(errorDiv) {
+		output.innerHTML = ''
+		errorDiv.innerHTML = errorMessage
 	}
 	
 	// Перебор введенного текста пользователем
@@ -40,14 +55,8 @@ window.onload = function () {
 		  
 			return count
 		} else {
-			showAlert()
-			return ''
+			return false
 		}
-	}
-	
-	// Отображение ошибки
-	function showAlert() {
-		alert(errorMessage)
 	}
 }
 
